710d3eb0d501f07c33278119317803b99f4605ea
chanakya-src <rc9917005174@gmail.com>
2025-10-04 13:43:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,29 +3,27 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-03c4f11b50838ab5d"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "terraform03_10north Virginia"
+    # REMOVE THIS LINE: key_name = "terraform03_10north Virginia"
     tags = {
         Name = "DevOps"
     }
-    
 }
-   
 
-
-  resource "aws_elb" "web_server_lb"{
-     name = "web-server-lb"
-     security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-0bf7e665914161460", "subnet-001c94845fdcc17e2"]
-     listener {
-      instance_port     = 80
-      instance_protocol = "http"
-      lb_port           = 80
-      lb_protocol       = "http"
+resource "aws_elb" "web_server_lb" {
+    name = "web-server-lb"
+    security_groups = [aws_security_group.web_server.id]
+    subnets = ["subnet-001c94845fdcc17e2", "subnet-0bf7e665914161460"]
+    listener {
+        instance_port     = 80
+        instance_protocol = "http"
+        lb_port           = 80
+        lb_protocol       = "http"
     }
     tags = {
-      Name = "terraform-elb"
+        Name = "terraform-elb"
     }
-  }
+}
+
 resource "aws_autoscaling_group" "web_server_asg" {
     name                 = "web-server-asg"
     min_size             = 1
@@ -33,12 +31,9 @@ resource "aws_autoscaling_group" "web_server_asg" {
     desired_capacity     = 2
     health_check_type    = "EC2"
     load_balancers       = [aws_elb.web_server_lb.name]
-    availability_zones    = ["us-east-1a", "us-east-1b"] 
+    vpc_zone_identifier  = ["subnet-001c94845fdcc17e2", "subnet-0bf7e665914161460"]
     launch_template {
         id      = aws_launch_template.web_server_as.id
         version = "$Latest"
-      }
-    
-    
-  }
-
+    }
+}
